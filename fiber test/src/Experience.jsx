b57ff54a934e6b584319203e3c285dde0e006aa0
Jose-Sal <jose.salcedo.386@my.csun.edu>
2023-04-28 19:15:11 -0700

    all planets implemented

--- fiber test/src/Experience.jsx
+++ fiber test/src/Experience.jsx
@@ -24,30 +24,38 @@
 // }
 //////////////////////////////////////////////////////////////////////////////////
 import {Sparkles,useTexture, useGLTF, OrbitControls, ScrollControls} from '@react-three/drei'
+import { Planets } from './components/Planets'
 
 export default function Experience()
 {
+    // get our model objects and convert to node
     const {nodes} = useGLTF('./models/alltogether.glb')
     console.log(nodes)
     const AlderaanTexture= useTexture('./models/Alderaan.jpg')
     AlderaanTexture.flipY=false
     console.log(AlderaanTexture)
     return<>
-
+        {/* have the background be a dark color */}
         <color args={['#030202']} attach="background"/>
+        {/* directional light to have a glare on the planet  */}
         <directionalLight position={[1,2,3]} intensity={1}/>
         {/* <ambientLight intensity={0.5} /> */}
+        {/* cannot zoom */}
         <OrbitControls enableZoom={false}/>
         <ambientLight intensity={1} />
+        {/* expect to scroll 3 times */}
         <ScrollControls pages={3} damping={0.25}>
             <mesh>
                 {/* <sphereGeometry /> */}
                 <meshStandardMaterial color={"green"}/>
             </mesh>
+            {/* we use spoarkles to replicate stars in the background */}
             <Sparkles size={6} scale={[200,200,400]} position-z={-5} speed={2} count={1000}/>
-            <mesh geometry={nodes.Alderaan.geometry}>
+            {/* create the geometry for our planets along with adding the materials with it */}
+            {/* <mesh geometry={nodes.Alderaan.geometry}>
                 <meshBasicMaterial map={AlderaanTexture} />
-            </mesh>
+            </mesh> */}
+            <Planets />
         </ScrollControls>
     </>
     
