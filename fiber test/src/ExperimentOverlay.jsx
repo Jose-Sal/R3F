dc5fd3ab722e521f8b1045dee31064a22d28f7b9
Jose-Sal <jose.salcedo.386@my.csun.edu>
2023-05-12 01:56:35 -0700

    update

--- fiber test/src/ExperimentOverlay.jsx
+++ fiber test/src/ExperimentOverlay.jsx
@@ -1,109 +1,100 @@
-import { Children } from 'react'
-import { motion } from 'framer-motion'
+import { Children } from "react";
+import { motion } from "framer-motion";
 // import { Logo } from '@pmndrs/branding'
-import { useStore } from './store'
-import { Center } from '@react-three/drei'
-import React, { useState, useEffect } from 'react'
+import { useStore } from "./store";
+import { Center } from "@react-three/drei";
+import React, { useState, useEffect } from "react";
 
+// if the object is not clicked on then it will stay hidden but once it's clicked then the opacity will show the text
 const item = {
-  hidden: { opacity: 0, y: '100%' },
-  show: { opacity: 1, y: 0 }
-}
+  hidden: { opacity: 0, y: "100%" },
+  show: { opacity: 1, y: 0 },
+};
 
+// this will check if the state is open through clicking the children mesh
 function List({ children, open }) {
   return (
-    <motion.ul  initial="hidden" animate={open ? 'show' : 'hidden'}>
+    // this will be the animation once the block is clicked
+    <motion.ul initial="hidden" animate={open ? "show" : "hidden"}>
       {Children.map(children, (child) => (
         <li>
+          {/* animate the child after the statement */}
           <motion.div variants={item}>{child}</motion.div>
         </li>
       ))}
     </motion.ul>
-  )
+  );
 }
 
+// what will be returned
 export function Overlay() {
-  const state = useStore()
-/////////////////////////////////////////////////////
-  const [filmData, setFilmData] = useState(null)
-
+  const state = useStore();
+  /////////////////////////////////////////////////////
+  // followed the tutorial for useState
+  const [filmData, setFilmData] = useState(null);
+  // fetch the data from the api
   useEffect(() => {
-    fetch('https://swapi.dev/api/films/')
-      .then(response => response.json())
-      .then(data => {
-        const films = data.results
-        const latestFilm = films[films.length - 1]
-        const { title, release_date } = latestFilm
-        setFilmData({ title, releaseDate: new Date(release_date).toLocaleDateString() })
-      })
-  }, [])
-/////////////////////////////////////////////////////
-
+    fetch("https://swapi.dev/api/films/")
+      .then((response) => response.json())
+      .then((data) => {
+        const films = data.results;
+        const latestFilm = films[films.length - 1];
+        const { title, release_date, opening_crawl } = latestFilm;
+        // send the data we gained from the fetch
+        setFilmData({
+          title,
+          releaseDate: release_date,
+          openingCrawl: opening_crawl,
+        });
+      });
+  }, []);
+  /////////////////////////////////////////////////////
 
   return (
     <>
-    {/* <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script> */}
-      <div style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%' }}>
-      <div style={{ position: 'absolute', top: 20, left: 25, fontSize: '25px' }}>
-        <a href="index.html">Go back</a>
-      </div>
-      <div style={{ position: 'absolute', top: 40, left: 100, fontSize: '25px' }}>Click on the Film</div>
+      {/* <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script> */}
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <div
+          style={{ position: "absolute", top: 20, left: 25, fontSize: "25px" }}
+        >
+          <a href="index.html" >Go back</a>
+        </div>
+        <div
+          style={{ position: "absolute", top: 40, left: 100, fontSize: "25px" }}
+        >
+          Click on the Film
+        </div>
       </div>
       <div className="info">
         <List open={state.open}>
-          <h3>Example</h3>
-{/* ---------------------------------------------------- */}
-        {filmData && (
-        <>
-          <p>Title: {filmData.title}</p>
-          <p>Release Date: {filmData.releaseDate}</p>
-        </>
-      )}
-{/* ---------------------------------------------------- */}
-            <div id='app'>
-                <h1>swapi</h1>
-            </div>
-
-            {/* <script>
-            var app = new Vue({
-                el: '#app',
-                data: {
-                    swapiData: {}
-                },
-                methods: {
-                    fetchSWAPI() {
-                    fetch('https://swapi.dev/api/people/1/')
-                        .then(response => response.json())
-                        .then(data => {
-                        this.swapiData = data;
-                        })
-                        .catch(error => console.error(error));
-                    }
-                },
-                created() {
-                    this.fetchSWAPI();
-                }
-                });
-            </script> */}
-          {/* <h3>“ZOOM”</h3> */}
-          {/* <h3> */}
-            {/* <span className="accent">PEGASUS</span> */}
-          {/* </h3> */}
-          {/* <h4>Running Shoes</h4> */}
-          {/* <p className="price">$98.97</p> */}
-          <p>
-            Year after year Pegasus has proven itself on the feet of runners everywhere. Now our most trusted style returns with new
-            innovations that make it more itself than ever. Meet the reliable, comfortable, always ready-to-run Nike Air Zoom Pegasus.
-          </p>
-          <p>
-            Year after year Pegasus has proven itself on the feet of runners everywhere. Now our most trusted style returns with new
-            innovations that make it more itself than ever. Meet the reliable, comfortable, always ready-to-run Nike Air Zoom Pegasus.
-          </p>
-          
+          {/* ---------------------------------------------------- */}
+          {filmData && (
+            <>
+              <p>Title: {filmData.title}</p>
+              <p>Release Date: {filmData.releaseDate}</p>
+              <p>Opening Crawl: {filmData.openingCrawl}</p>
+            </>
+          )}
+          {/* ---------------------------------------------------- */}
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/5UnjrG_N8hU"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            allowfullscreen
+          ></iframe>
         </List>
-
       </div>
-      
     </>
-  )
+  );
 }
