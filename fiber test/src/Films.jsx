55962d5f9bcc733f9b04dfd56b49501ece7b7083
Jose-Sal <jose.salcedo.386@my.csun.edu>
2023-05-18 12:45:22 -0700

    update

--- fiber test/src/Films.jsx
+++ fiber test/src/Films.jsx
@@ -17,44 +17,4 @@ root.render(
            
         </Canvas>
         
-        // <Interface />
-    // </>
 )
-
-// export default function Films(){
-//     return(
-//         <Canvas camera={{position: [0,1,5]}} >
-//              <color args={['#030202']} attach ="background"/>
-//          {/* directional light to have a glare on the planet  */}
-//          <directionalLight position={[1,2,3]} intensity={1}/>
-//          <Sparkles size={6} scale={[200,200,400]}  speed={2} count={1000}/>
-//              {/* creates the object*/}
-//              <mesh  position={[-2,0,0]}>
-//                  <boxGeometry />
-//                  <meshStandardMaterial color={"green"}/>
-//              </mesh>
-//              <mesh  position={[0,0,0]}>
-//                  <boxGeometry />
-//                  <meshStandardMaterial color={"green"}/>
-//              </mesh>
-//              <mesh  position={[2,0,0]}>
-//                  <boxGeometry />
-//                  <meshStandardMaterial color={"green"}/>
-//              </mesh>
-//              <mesh  position={[-2,-2,0]}>
-//                  <boxGeometry />
-//                  <meshStandardMaterial color={"green"}/>
-//              </mesh>
-//              <mesh  position={[0,-2,0]}>
-//                  <boxGeometry />
-//                  <meshStandardMaterial color={"green"}/>
-//              </mesh>
-//              <mesh  position={[2,-2,0]}>
-//                  <boxGeometry />
-//                  <meshStandardMaterial color={"green"}/>
-//              </mesh>
-//              {/* <Interface /> */}
-//          {/* <FilmOverlay /> */}
-//          </Canvas>
-//     )
-// }
